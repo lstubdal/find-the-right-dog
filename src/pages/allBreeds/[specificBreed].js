1a2ff539ae9c7d8c9de388083b0d614d3097c19c
lstubdal <lotte.stubdal@gmail.com>
2023-04-22 16:10:06 +0100

    Specific brees page styling

--- src/pages/allBreeds/[specificBreed].js
+++ src/pages/allBreeds/[specificBreed].js
@@ -1,12 +1,13 @@
 import Header from '../../components/header/header';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { useRouter } from 'next/router';
+import styles from '@component/styles/SpecificBreed.module.css';
 
-/* import Height from '../dogInformation/height';
-import Lifespan from '../dogInformation/lifespan';
-import Weight from '../dogInformation/weight' */
+import Height from '@component/components/dogInformation/height';
+import Lifespan from '@component/components/dogInformation/lifespan';
+import Weight from '@component/components/dogInformation/weight';
 
- const SpecificBreed = ({ specificBreed })  => {
+const SpecificBreed = ({ specificBreed })  => {
 
     // avoid website to render data before api fetch is complete
     const router = useRouter() // access router hook
@@ -15,56 +16,58 @@ import Weight from '../dogInformation/weight' */
     }
 
     return (
-        <div className='specificBreed'>
-            <Header />
-            <div className='dog'>
-                <img className="dog__img" src={`${specificBreed[0].image_link}`} alt={`$specificBreed[0].name`} />
-                <h3 className='dog__name'>{specificBreed[0].name}</h3>
+        <div className={styles.specificBreed}>
+           <Header />
+            <div className={styles.dog}>
+                <img className={styles.dog__img} src={`${specificBreed[0].image_link}`} alt={`$specificBreed[0].name`} />
+                <h3 className={styles.dog__name}>{specificBreed[0].name}</h3>
 
-               {/*  <section className="info">
-                    <div className='info__details'>
-                        <div className='info__details-detail'>
+                <section className={styles.info}>
+                    <div className={styles.info__details}>
+                        <div className={styles.info__detailsItem}>
                             <Lifespan />
                             <p>Lifespan</p>
                         </div>
                         <p>{specificBreed[0].min_life_expectancy} - {specificBreed[0].max_life_expectancy} years</p>
                     </div>
-                    <div className='info__details'>
-                        <div className='info__details-detail'>
+
+                    <div className={styles.info__details}>
+                        <div className={styles.info__detailsItem}>
                             <Height />
                             <p>Height</p>
                         </div>
                         <p>F: {specificBreed[0].min_height_female} in</p>
                         <p>M: {specificBreed[0].max_height_male} in</p>
                     </div>
-                    <div className='info__details'>
-                        <div className='info__details-detail'>
+
+                    <div className={styles.info__details}>
+                        <div className={styles.info__detailsItem}>
                             <Weight />
                             <p>Weight</p>
                         </div>
                         <p>F: {specificBreed[0].min_weight_female}kg - {specificBreed[0].max_weight_female}kg</p>
                         <p>M: {specificBreed[0].min_weight_male}kg - {specificBreed[0].max_weight_male}kg</p>
                     </div>
-                </section> */}
+                </section>
 
-                <p className='dog__description'>description (bred for also) from another api</p>
+                <p className={styles.dog__description}>description (bred for also) from another api</p>
                 
-                <p className='info'>More information about {specificBreed[0].name}, shown on a scale where 0 is the lowest and 5 is the highest</p>
-                <section className='info'>
-                    <div className='info__details'>
+                <p className={styles.info}>More information about {specificBreed[0].name}, shown on a scale where 0 is the lowest and 5 is the highest</p>
+                <section className={styles.info}>
+                    <div className={styles.info__detailsItem}>
                         <h4>PERSONALITY TRAITS</h4>
                         <p>Energy: {specificBreed[0].energy}</p>
                         <p>Playfullness: {specificBreed[0].playfulness}</p>
                         <p>Trainability: {specificBreed[0].trainability}</p>
                         <p>Protectiveness: {specificBreed[0].protectiveness}</p>
                     </div>
-                    <div className='info__details'>
+                    <div className={styles.info__detailsItem}>
                         <h4>BEHAVOUIR</h4>
                         <p>Good with children: {specificBreed[0].good_with_children}</p>
                         <p>Good with other dogs: {specificBreed[0].good_with_other_dogs}</p>
                         <p>Good with strangers: {specificBreed[0].strangers}</p>
                     </div>
-                    <div className='info__details'>
+                    <div className={styles.info__detailsItem}>
                         <h4>GROOMING</h4>
                         <p>Grooming: {specificBreed[0].grooming}</p>
                         <p>Shedding: {specificBreed[0].shedding}</p>
@@ -90,7 +93,7 @@ export async function getStaticPaths() {
         }
     }))
 
-    return {paths, fallback: false}
+    return { paths, fallback: true } // whats the difference
 }
 
 export async function getStaticProps({ params }) {
@@ -109,42 +112,4 @@ export async function getStaticProps({ params }) {
             specificBreed
         }
     }
-
-/*     const [dogBreed, setDogBreed] = useState(null);
-    const [loading, setLoading ] = useState(true)
-
-    useEffect(() => {
-        // access the last param of url for specific fetching
-        // source inspiration: https://stackoverflow.com/questions/4758103/last-segment-of-url-with-javascript 
-        const currentPath = window.location.pathname;
-        const currentDogBreed = currentPath.substring(currentPath.lastIndexOf('/') + 1)
-    
-        // url for information about dog user clicked on on 'all breeds page'
-        const url = `https://api.api-ninjas.com/v1/dogs?name=${currentDogBreed}`
-
-        // fetch with api key
-        const fetchDogBreed = async () => {
-            try {
-                const response = await fetch(url, {
-                    headers: {
-                        // put in .ENV file!
-                        "x-api-key" : "ObaGZREF0xBS2FWkE2ecKTUNk79EdhGvN8ENB5cZ"
-                    }
-                });
-
-                const data  = await response.json()
-                setDogBreed(data);
-                setLoading(false); // make sure data is updated 
-                console.log(data)
-                
-            } catch(error) {
-                console.log('error specifiv dogBreed page', error)
-            }
-        }
-
-        fetchDogBreed() 
-    }, []) */
-
-    
-    
 }
