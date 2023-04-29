8a84620c920a4c8e03ce0326c554e45a3bd5e46d
lstubdal <lotte.stubdal@gmail.com>
2023-04-29 13:36:16 +0100

    Display questions with options dynamicly

--- src/pages/findBreed.js
+++ src/pages/findBreed.js
@@ -1,23 +1,41 @@
 import React from "react";
 import { useSelector, useDispatch } from 'react-redux';
+import styles from '@component/styles/FindBreed.module.css';
 
 export default function FindBreed() {
-    const index = useSelector((state) => state.quiz.index);
-    const questions = useSelector((state) => state.quiz.questions ) // access questions for user from redux
-    
-    console.log('questions', questions)
-    console.log(index);
 
+    // access slice from redux 
+    const index = useSelector((state) => state.quiz.index); 
+    const questions = useSelector((state) => state.quiz.questions); 
+
+    // make reducers accessbible to current component
+    const dispatch = useDispatch();
 
     return(
-        <div>
+        <div className={styles.findBreed}>
             <h2>DOG BREED SELECTOR</h2>
             <p>Answer a few questions about your expections of your future four legged friend</p>
 
-            <section>
-            {questions.map((question) => (
-                <h3 key={question.id}>{question.question}</h3>
-            ))}
+            <section className={styles.quiz}>
+                <p>Question {index + 1}/10</p>
+                <h3>{questions[index].question}</h3>
+
+                <div>
+                    <input type="checkbox" name="low" />
+                    <label for="low">{questions[index].option1}</label>
+
+                    <input type="checkbox" name="medium" />
+                    <label for="medium">{questions[index].option2}</label>
+
+                    <input type="checkbox" name="high" />
+                    <label for="high">{questions[index].option3}</label>
+                </div>
+
+                <div>
+                    <button>back</button>
+                    <button>next</button>
+                </div>
+
             </section>
         </div>
     )
