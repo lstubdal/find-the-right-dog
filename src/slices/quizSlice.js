3194c28e0852f7c0f876c7d0e3cdb98b289bde58
lstubdal <lotte.stubdal@gmail.com>
2023-05-01 16:49:40 +0100

    Add all the quiz questions

--- src/slices/quizSlice.js
+++ src/slices/quizSlice.js
@@ -31,6 +31,69 @@ const initialState = {
                 {option: 'Yes, with children'},
             ]
         },
+        { // good_with_other_dogs
+            id: 'good_with_other_dogs',
+            question: 'Is it important that the dog is friendly with other dogs',
+            options: [
+                {option: 'Not that important'},
+                {option: 'Matters a little'},
+                {option: 'Yes, important'},
+            ]
+        },
+        { //good_with_strangers
+            id: 'good_with_strangers',
+            question: 'Is it important that the dog is friendly with strangers?',
+            options: [
+                {option: 'Not that important'},
+                {option: 'Matters a little'},
+                {option: 'Yes, important'},
+            ]
+        },
+        { // playfullness
+            id: 'playfullness',
+            question: 'How much will your dog be able to play with you?',
+            options: [
+                {option: 'Once a week or less'},
+                {option: 'Around 2-4 days a week'},
+                {option: 'Everyday or more'},
+            ]
+        },
+        { // independence
+            id: 'independence',
+            question: 'Will your dog spend much time alone on daily basis?',
+            options: [
+                {option: 'It will be with someone most of the time'},
+                {option: 'Around 4-6 hours'},
+                {option: 'Until i get home from work (8hours+)'},
+            ]
+        },
+        { // trainability
+            id: 'trainability',
+            question: 'How trainable do you want your dog to be',
+            options: [
+                {option: 'Not that important'},
+                {option: 'I prefer trainabile'},
+                {option: 'Highly trainabile'},
+            ]
+        },
+        { // grooming
+            id: 'grooming',
+            question: 'How much time will you have for grooming?',
+            options: [
+                {option: 'Not much time'},
+                {option: 'Every so often'},
+                {option: 'Daily grooming'},
+            ]
+        },
+        { // shedding
+            id: 'shedding',
+            question: 'How much shedding are you okay with?',
+            options: [
+                {option: 'Minimal shedding'},
+                {option: 'Some shedding is okay'},
+                {option: 'Not important'},
+            ]
+        }
     ],
 
     answers: []
