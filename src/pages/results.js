3fcc0b35bd735fdc3d5aca83dfefa82f2b0a6381
Lotte Stubdal <lotte.stubdal@gmail.com>
2023-05-03 23:35:27 +0100

    last commit windows comp

--- src/pages/results.js
+++ src/pages/results.js
@@ -3,8 +3,18 @@ import { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import Header from '../components/header/header';
 
-export default function ResultsFindBreed() {
-    const allBreeds = useSelector((state) => state.fetchedData.data);
+export default function Results() {
+    const allBreeds = useSelector((state) => state.fetchedData.data); // access all dogbreeds froms store
+    const answers = useSelector((state) => state.quiz.answers)        // access answers from quiz saved in store
+
+    console.log('answers' , answers.length);
+    console.log('dogs', allBreeds.length); // handle if answer = 0
+
+
+    answers.forEach(answer => {
+        console.log(answer)
+    })
+
     
     
 
