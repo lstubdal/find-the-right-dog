171f2151a97f80e21aea16e72011309d2cbc7f14
Lotte Stubdal <lotte.stubdal@gmail.com>
2023-05-03 22:27:24 +0100

    update filename

--- src/pages/results.js
+++ src/pages/results.js
@@ -1,8 +1,11 @@
 import React from "react";
 import { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import Header from '../components/header/header';
 
 export default function ResultsFindBreed() {
+    const allBreeds = useSelector((state) => state.fetchedData.data);
+    
     
 
     return (
