305263c4c6013fa979da005b4e6df74c675ad50e
Lotte Stubdal <lotte.stubdal@gmail.com>
2023-05-04 11:43:21 +0100

    switchcase for sizerange filter

--- src/pages/results.js
+++ src/pages/results.js
@@ -14,6 +14,55 @@ export default function Results() {
     answers.forEach(answer => {
         console.log(answer)
     })
+    
+
+    function returnSize(input) {
+        let sizeRange = []
+        switch(input) {
+            case 1: {
+                return sizeRange = [0, 15];
+            }
+            case 2: {
+                return sizeRange = [16, 30];
+            }
+            case 3: {
+                return sizeRange = [31, 50];
+            }
+            case 4: {
+                return sizeRange = [51, 109];
+            }
+            case 5: {
+                return sizeRange = [110, 200];
+            }
+        }
+    }
+
+    getCurrentDay() {
+        let day = 0;
+        switch(new Date().getDay()){
+            case 0: {
+                return day = 0;
+            }
+            case 1: {
+                return day = 1;
+            }
+            case 2: {
+                return day = 2;
+            }
+            case 3: {
+                return day = 3;
+            }
+            case 4: {
+                return day = 4;
+            } 
+            case 5: {
+                return day = 5;
+            }
+            case 6: {
+                return day = 6;
+            }
+        }
+    }
 
     
     
