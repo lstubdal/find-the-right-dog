4267c0524bcd9a57bd3a9a8c750a972f8b9565f9
Lotte Stubdal <lotte.stubdal@gmail.com>
2023-05-04 12:29:49 +0100

    Filter by size

--- src/pages/results.js
+++ src/pages/results.js
@@ -1,4 +1,5 @@
 import React from "react";
+import * as filter  from "../filterFunctions"
 import { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import Header from '../components/header/header';
@@ -6,67 +7,20 @@ import Header from '../components/header/header';
 export default function Results() {
     const allBreeds = useSelector((state) => state.fetchedData.data); // access all dogbreeds froms store
     const answers = useSelector((state) => state.quiz.answers)        // access answers from quiz saved in store
-
-    console.log('answers' , answers.length);
-    console.log('dogs', allBreeds.length); // handle if answer = 0
-
-
-    answers.forEach(answer => {
-        console.log(answer)
-    })
+    let currentFilteredResult = []
     
+    // filter by size
+    const currentSize = filter.bySize(answers[0].answer)
+    currentFilteredResult = allBreeds.filter(dog => dog.max_weight_male >= currentSize[0] && dog.max_weight_male <= currentSize[1]);
+    console.log("by size: ", currentFilteredResult.length)
 
-    function returnSize(input) {
-        let sizeRange = []
-        switch(input) {
-            case 1: {
-                return sizeRange = [0, 15];
-            }
-            case 2: {
-                return sizeRange = [16, 30];
-            }
-            case 3: {
-                return sizeRange = [31, 50];
-            }
-            case 4: {
-                return sizeRange = [51, 109];
-            }
-            case 5: {
-                return sizeRange = [110, 200];
-            }
-        }
-    }
+    //filter by energy
 
-    getCurrentDay() {
-        let day = 0;
-        switch(new Date().getDay()){
-            case 0: {
-                return day = 0;
-            }
-            case 1: {
-                return day = 1;
-            }
-            case 2: {
-                return day = 2;
-            }
-            case 3: {
-                return day = 3;
-            }
-            case 4: {
-                return day = 4;
-            } 
-            case 5: {
-                return day = 5;
-            }
-            case 6: {
-                return day = 6;
-            }
-        }
-    }
 
-    
-    
+   
 
+
+ 
     return (
         <div>
             <Header />
@@ -75,3 +29,4 @@ export default function Results() {
         </div>
     )
 }
+
