e26c8923d5ab9744f27791412f2dc45217302d05
Lotte Stubdal <lotte.stubdal@gmail.com>
2023-05-05 15:31:01 +0100

    Content, styling and responsive

--- src/pages/helpPage.js
+++ src/pages/helpPage.js
@@ -1,9 +1,16 @@
 import Header from '../components/header/header';
+import styles from '@component/styles/HelpPage.module.css'
 
 export default function HelpPage() {
     return(
-        <div>
+        <div className={styles.helpPage}>
             <Header />
+            <h2 className={styles.helpPage__title}>What is this page?</h2>
+
+            <section className={styles.helpPage__content}>
+                <img src='/images/dog_and_owner.png' alt='dog and owner' className={styles.helpPage__contentItem} />
+                <p className={styles.helpPage__contentParagraph}>Answer a few questions to see what type of dog breeds that might suit Answer a few questions to see what type of dog breeds that might suit Answer a few questions to see what type of dog breeds that might suit Answer a few questions to see what type of dog breeds that might suit Answer a few questions to see what type of dog breeds that might suit </p>
+            </section>
         </div>
     )
 }
